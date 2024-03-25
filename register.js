1873ed7468bbfc944ae5a0892d2d32752843bde4
lri28 <leonarose.ilao@gmail.com>
2024-03-24 18:24:32 -0600

    update register screen
    update register screen

--- register.js
+++ register.js
@@ -2,7 +2,7 @@ function $$(name){
     return document.getElementById(name);
 }
 
-//Function to register.  Data will be saved on an array.
+// Function to register. Data will be saved on an array.
 function RegisterAccount(){
     var fullname = $$('reg-fullname');
     var email = $$('reg-email');
@@ -10,6 +10,7 @@ function RegisterAccount(){
     var username = $$('reg-username');
     var password = $$('reg-password');
     var acces = $$('reg-useraccess');
+    var successRegister = $$('successRegister');
 
     var data = {
         fullname: fullname.value,
@@ -22,19 +23,43 @@ function RegisterAccount(){
     var jsonString = JSON.stringify(data);
     console.log(jsonString)
 
+    // Check if all required fields are filled
+    if (fullname.value && email.value && phone.value && username.value && password.value && acces.value) {
+        // Display success message
+        successRegister.textContent = "Successfully Registered!";
+        successRegister.style.color = "black";
+        successRegister.style.display = "block";
+
+        // Automatically remove success message after 5 seconds
+        setTimeout(function() {
+            successRegister.textContent = "";
+            successRegister.style.display = "none";
+        }, 5000);
+    
+
+    // Clear input fields
     fullname.value = "";
     email.value = "";
     phone.value = "";
     username.value = "";
     password.value = "";
     acces.selectedIndex = -1;
+} else {
+    // Display error message
+    successRegister.textContent = "Please fill all fields!";
+    successRegister.style.color = "red";
+    successRegister.style.display = "block";
+
+    // Automatically remove error message after 3 seconds
+    setTimeout(function() {
+        successRegister.textContent = "";
+        successRegister.style.display = "none";
+    }, 3000);
+}
 }
 
-//Function to go back to Homepage
+
+// Function to go back to Homepage
 function RegisterCancel(){
     window.location.href = "index.html";
 }
-
-
-
-
